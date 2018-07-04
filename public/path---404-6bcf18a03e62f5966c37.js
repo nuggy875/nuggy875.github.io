bb8743a69b8eaa3578e9282cc4686de63d7ab945
nuggy875 <jinugpaeng@gmail.com>
2018-07-04 22:05:37 +0900

    initialize

--- public/path---404-6bcf18a03e62f5966c37.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xe70826b53c04],{401:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Blog by Gilbert Paeng",subtitle:"Computer Vision / Android(Java) / React / NodeJS / 이것저것",copyright:"© All rights reserved.",menu:[{label:"Articles",path:"/"},{label:"About me",path:"/about/"},{label:"Contact me",path:"/contact/"}],author:{name:"Gilbert Paeng",email:"jinugpaeng@gmail.com",telegram:"#",instagram:"paeng___",github:"nuggy875",rss:"#",vk:"#"}}}},pathContext:{}}}});
-//# sourceMappingURL=path---404-6bcf18a03e62f5966c37.js.map
